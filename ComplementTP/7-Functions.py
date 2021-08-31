6f99d8f40637a9ef425376318faf02f49cc98ee7
rednaks69 <hasnaoui.azer@gmail.com>
2021-08-31 01:12:58 +0100

    v3.1

---
+++ ComplementTP/7-Functions.py
@@ -0,0 +1,61 @@
+####################################################################################################
+# * Complement de TP python
+# * made by : Azer Hasnaoui
+# * classroom : GLSI-1
+# * year : 2020-2021
+####################################################################################################
+#                     -functions-                                                                  #
+####################################################################################################
+# ? Summary
+# ? -1- Function Definition
+# ? -2- Function Call
+# ? -3- Positional Arguments
+# ? -4- Default Arguments
+# ? -5- Keyword Arguments
+# ? -----5-1- Keyword Arguments for Positional Arguments
+# ? -----5-2 Keyword Arguments for Default Arguments
+# ? -6-  Variable Arguments
+# ? -----6-1- Variable Arguments With Positional Parameters
+# ? -----6-2- Variable Arguments With Default Arguments
+# ? -----6-3- Variable Arguments Followed by Default Arguments
+# ? -----6-4- Variable Arguments Followed by Keyword Arguments
+# ? -7- Returning From Functions
+# ? -8- Returning Single Values From Functions
+# ? -9- Returning Collections From Functions
+# ? -----9-1- Returning Tuples From Functions
+# ? -----9-2- Returning Lists From Functions
+# ? -----9-3- Returning Dictionaries From Functions
+# ? -10- Global VariablesGlobal Variables
+# ? -11- Nested Functions
+# ? -----11-1- Inner Functions for Private Use
+# ? -----11-2- Outer Functions for Abstraction
+# ? -----11-3- Outer Functions as Generators
+# ? -12- Lambda Expressions
+# ? -----12-1- Anonymous functions
+# ? -----12-2- Lambda
+# ? -13- Unpacking Argument Lists
+####################################################################################################
+# * -1- Function Definition
+"""
+def function_name(parameters):
+    function_body
+    ...
+"""
+
+# * -2- Function Call
+
+
+def hi():
+    print("Hello World")
+
+
+hi()
+
+# * -3- Positional Arguments
+
+
+def sum(x, y):
+    print(x+y)
+
+
+sum(2, 3)
